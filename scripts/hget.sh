35c001043fda13179d45ca9d84d8a6348df51d09
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2016-07-26 16:17:40 +0300

    Create hget.sh

---
+++ scripts/hget.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# hget - HTTP/S Client Simulator
+# Artiom Lichtenstein
+# v1.3, 22/12/2013
+
+trap "echo; echo 'Exiting...'; rm -f scr_cURL_HGET.cookies; exit" SIGINT
+
+if [ -z "$1" ]; then
+  echo; echo "Usage: ./hget http|s://{HOST_NAME | IP_ADDRESS}[:PORT][/URI] [INTERVAL]"; echo
+  exit
+fi
+
+int_TRANS_NUM=1
+str_BASE_URI="/url"
+str_FILE="index.php"
+str_URL="$1"
+str_SLASHES="${1//[^\/]}"
+
+if [ -z "$2" ]; then set -- $1 1; fi
+
+while true 
+ do
+      if [ ${#str_SLASHES} -le 2 ]; then
+         str_URL="$1$str_BASE_URI$(( ($RANDOM % 3) + 1 ))/"
+         if [ $(($RANDOM % 3)) = 0 ]; then 
+           str_URL=$str_URL$str_FILE
+         fi
+      fi
+   clear
+   echo
+   echo $(tput sgr0)$(tput bold)Transaction Number: $(tput setaf 2)$(tput bold)$int_TRANS_NUM $(tput sgr0)$(tput bold)to: $(tput setaf 6)$str_URL$(tput sgr0)
+   curl -A hget_v1.3 -G $str_URL -k -v -s -b scr_cURL_HGET.cookies -c scr_cURL_HGET.cookies -o /dev/null
+   int_TRANS_NUM=$((int_TRANS_NUM+1))
+   sleep $2
+ done
