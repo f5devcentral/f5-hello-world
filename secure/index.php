b473c53238596b3c0f3f01084f56653eb5b337b7
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2017-12-10 02:16:06 +0200

    Auth Subpage

--- secure/index.php
+++ secure/index.php
@@ -27,7 +27,7 @@
 		}
 	</script>
 	<link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
-	<link href="..css/skeleton.min.css" rel="stylesheet" type="text/css">
+	<link href="../css/skeleton.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 	<div class="container">
