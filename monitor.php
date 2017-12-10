b0ba0d609a82b8289816b062643057afbe8efdda
Artiom Lichtenstein <ArtiomL@users.noreply.github.com>
2017-12-10 02:55:24 +0200

    Health Monitor

--- monitor.php
+++ monitor.php
@@ -1,3 +1,5 @@
-<?php
-	echo "Hello ADCs! PHP is successfully running v".phpversion()." :-)";
-?>
+<?php header("content-type: text/plain"); ?>
+<?php echo "Hello ADCs! PHP is successfully running v".phpversion()." :)\n"; ?>
+http.host: <?php echo $_SERVER["HTTP_HOST"],"\n"; ?>
+<?php if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) { echo "http.x_forwarded_for: ", $_SERVER["HTTP_X_FORWARDED_FOR"]; } else { echo "ip.src: ", $_SERVER["REMOTE_ADDR"]; } ?> 
+ip.dst: <?php echo $_SERVER["SERVER_ADDR"]; ?>
